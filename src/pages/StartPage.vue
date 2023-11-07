e06f2c96778ca28b461951465909ae9da192f7f6
Dmitriy <dfajl@mail.ru>
2023-11-07 10:51:50 +0300

    styles

--- src/pages/StartPage.vue
+++ src/pages/StartPage.vue
@@ -24,10 +24,10 @@
 </script>
 
 <style lang="scss" scoped>
-	body {
+	/* body {
 		background: var(--tg-theme-bg-color);
 		color: var(--tg-theme-text-color);
-	}
+	} */
 	.startPageWrapper {
 		display: flex;
 		flex-direction: column;
