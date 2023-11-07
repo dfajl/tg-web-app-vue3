c60e383ecff033c0672315c72f6abef8c5cbd97d
Dmitriy <dfajl@mail.ru>
2023-11-07 17:15:53 +0300

    dgdgdgd

--- src/pages/StartPage.vue
+++ src/pages/StartPage.vue
@@ -1,69 +1,36 @@
-<script setup>
-	import useTelegram from '@/hooks/useTelegram';
-	import { ref, onMounted, onBeforeUnmount, watchEffect } from 'vue';
-	import { useStore } from 'vuex';
-	import { useRoute } from 'vue-router';
-	//import { mapActions, mapState, mapMutations } from 'vuex';
-
-	let userData = ref(null);
-	//onMounted(() => {
-	const { tg, user } = useTelegram();
-	function showDataUser() {
-		userData.value = 'string';
-	}
-
-	//export default {
-	//setup() {
-	//let userData = ref(null);
-	//onMounted(() => {
-	//const { tg, user } = useTelegram();
-	//function showDataUser() {
-	//	userData.value = 'string';
-	//}
-
-	//});
-	/* onMounted(() => {
-				window.Telegram.WebApp.MainButton.show();
-				window.Telegram.WebApp.onEvent(
-					'mainButtonClicked',
-					handleMainButtonClick,
-				);
-			}); */
-
-	/* watchEffect(() => {
-				tg.ready();
-				window.Telegram.WebApp.MainButton.show();
-			});
-
-			const handleMainButtonClick = () => {
-				// Обработка события нажатия на кнопку "mainButton"
-				console.log('Нажата кнопка mainButton');
-				// Дополнительные действия, которые вы хотите выполнить при нажатии на кнопку
-			}; */
-	//return { tg, showDataUser, userData, user };
-	//},
-	//};
-</script>
-
 <template>
 	<div class="startPageWrapper">
 		<div class="logoWrapper">
 			<img src="../assets/images/neovox_logo.svg" alt="neovox_logo" />
 		</div>
-		<span>{{ tg }}</span>
-		<my-main-button class="startPageBtn" @click="showDataUser"
-			>Магазин</my-main-button
-		>
-		<span>{{ userData }}</span>
+		<span> {{ telegram }}</span>
+		<my-main-button class="startPageBtn">Магазин</my-main-button>
 		<my-main-button class="startPageBtn">Задания</my-main-button>
 	</div>
 </template>
 
+<script>
+	import useTelegram from '@/hooks/useTelegram';
+	import { ref, onMounted, onBeforeUnmount } from 'vue';
+	import { useStore } from 'vuex';
+	import { useRoute } from 'vue-router';
+	//import { mapActions, mapState, mapMutations } from 'vuex';
+	export default {
+		setup() {
+			let telegram = ref(null);
+			const { tg } = useTelegram();
+			telegram.value = tg.MainButton.color;
+
+			return { telegram };
+		},
+	};
+</script>
+
 <style lang="scss" scoped>
-	/* body {
+	body {
 		background: var(--tg-theme-bg-color);
 		color: var(--tg-theme-text-color);
-	} */
+	}
 	.startPageWrapper {
 		display: flex;
 		flex-direction: column;
@@ -75,7 +42,7 @@
 			display: flex;
 			align-self: center;
 			margin-bottom: 80px;
-			width: 65%;
+			width: 80%;
 			img {
 				width: 100%;
 			}
