88198ceada00104efc3d5bf98967fa372b141759
Dmitriy <dfajl@mail.ru>
2023-11-07 11:58:25 +0300

    app vue

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@
 </template>
 
 <script>
+	import useTelegram from '@/hooks/useTelegram';
 	import styles from './assets/styles/style.module.css';
 	export default {
 		name: 'App',
 		components: {},
+		setup() {
+			useTelegram();
+		},
 	};
 </script>
 
